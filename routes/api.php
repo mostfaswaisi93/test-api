bc053a4753d56f0ef10fac678c47ca7da9fc6584
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-05-17 23:43:32 +0300

    Added All

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/person', 'Api\v1\PersonController')
+        ->only(['show', 'destroy', 'update', 'store']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::apiResource('/people', 'Api\v1\PersonController')
+        ->only('index');
+});
+
+Route::prefix('v2')->group(function () {
+    Route::apiResource('/person', 'Api\v2\PersonController')
+        ->only('show');
 });
